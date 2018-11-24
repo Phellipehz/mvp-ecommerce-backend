c71fa65fae1157f1814e94fbcbd3074f076d5071
Phellipehz <phellipeh@alu.ufc.br>
2018-11-24 00:05:55 -0300

    Implementação Order

---
+++ src/main/java/com/ecommerce/backend/application/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.ecommerce.backend.application.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.backend.application.model.Order;
+import com.ecommerce.backend.application.service.OrderService;
+
+@RestController
+@RequestMapping(value ="/order", produces = "application/json")
+public class OrderController {
+
+	@Autowired(required=true)
+	OrderService oService;
+	
+	@RequestMapping(path = "", method = RequestMethod.GET)
+	public ResponseEntity<List<Order>> listOrders(){
+		List<Order> rOrder = oService.listAll();
+		return new ResponseEntity<>(rOrder, HttpStatus.OK);
+	}
+
+	@RequestMapping(path = "", method = RequestMethod.POST)
+	public ResponseEntity<Order> createOrder(@Valid @RequestBody Order order){	
+		Order rOrder = oService.create(order);
+		return new ResponseEntity<>(rOrder, HttpStatus.OK);
+	}
+	
+}
--- src/main/java/com/ecommerce/backend/application/model/Order.java
+++ src/main/java/com/ecommerce/backend/application/model/Order.java
@@ -1,11 +1,46 @@
 package com.ecommerce.backend.application.model;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Min;
 
+import com.ecommerce.backend.base.account.model.Account;
 import com.ecommerce.backend.base.account.model.BaseEntity;
 
-public class Order {
-
+@Entity
+public class Order extends BaseEntity {
 	
+	@OneToOne
+	Product product;
+	
+	@Min(1)
+	Long amount;
+	
+	@OneToOne
+	Account account;
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
 	
 }
